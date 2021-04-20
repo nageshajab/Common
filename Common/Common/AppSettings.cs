24f9618df644e498972971c457c853863f88323f
Nagesh Ajab <nagesh.ajab@synerzip.com>
2021-04-20 09:56:50 +0530

    .gitignore is now working

---
+++ Common/Common/AppSettings.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace Common
+{
+    public class AppSettings
+    {
+        public static IConfigurationRoot Root
+        {
+            get
+            {
+                var configurationBuilder = new ConfigurationBuilder();
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+                configurationBuilder.AddJsonFile(path, false);
+
+                var root = configurationBuilder.Build();
+                return root;
+            }
+        }
+    }
+}
