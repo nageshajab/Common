beef2dc37908f2000bf7ffc02ee930b80c5e04bd
nageshajab <ajab.nagesh@gmail.com>
2022-01-30 16:31:54 +0530

    a

--- Common/Common/Console.cs
+++ Common/Common/Console.cs
@@ -1,27 +1,31 @@
 using System;
-public class Console
+namespace CSharp.CommonUtilities
 {
-    public void ListMenus(string[] arr)
+    public class Console
     {
-        int i = 0;
-        foreach (string str in arr)
-        {
-            i += 1;
-            System.Console.WriteLine($"{i} {str}");
+        public static void ListMenus(string[] arr)
+        {            
+            System.Console.WriteLine("Enter your choince");
+            int i = 0;
+            foreach (string str in arr)
+            {
+                i += 1;
+                System.Console.WriteLine($"{i} {str}");
+            }
         }
-    }
-    public int ReadInt(string msg)
-    {
-        System.Console.WriteLine(msg);
-        string input= System.Console.ReadLine();
-        int output=0;
-        int.TryParse(input,out output);
-        return output;
-    }
-     public string ReadString(string msg)
-    {
-        System.Console.WriteLine(msg);
-        string input= System.Console.ReadLine();
-        return input;
+        public static int ReadInt(string msg)
+        {
+            System.Console.WriteLine(msg);
+            string input = System.Console.ReadLine();
+            int output = 0;
+            int.TryParse(input, out output);
+            return output;
+        }
+        public static string ReadString(string msg)
+        {
+            System.Console.WriteLine(msg);
+            string input = System.Console.ReadLine();
+            return input;
+        }
     }
 }
